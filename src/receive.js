df1f5b94c34f70e76423725e4e613ff303deed84
Daniela ION <daniela.ion@stud.acs.upb.ro>
2022-01-09 16:38:54 +0200

    It works

--- src/receive.js
+++ src/receive.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-
+const puppeteer = require('puppeteer');
 var amqp = require('amqplib/callback_api');
 var amqpConn = null;
 
-function start() {
+function startRabbitMQ(browser) {
     amqp.connect('amqp://localhost', function(err, conn) {
       if (err) {
         console.error("[AMQP]", err.message);
-        return setTimeout(start, 1000);
+        return setTimeout(startRabbitMQ(browser), 1000);
       }
       conn.on("error", function(err) {
         if (err.message !== "Connection closing") {
@@ -16,16 +16,16 @@ function start() {
       });
       conn.on("close", function() {
         console.error("[AMQP] reconnecting");
-        return setTimeout(start, 1000);
+        return setTimeout(startRabbitMQ(browser), 1000);
       });
       console.log("[AMQP] connected");
       amqpConn = conn;
-      startWorker();
+      startWorker(browser);
     });
   }
 
 // A worker that acks messages only if processed successfully
-function startWorker() {
+function startWorker(browser) {
     amqpConn.createChannel(function(err, ch) {
       if (closeOnErr(err)) return;
       ch.on("error", function(err) {
@@ -38,15 +38,21 @@ function startWorker() {
       ch.prefetch(10);
       ch.assertQueue("accounts", { durable: true }, function(err, _ok) {
         if (closeOnErr(err)) return;
-        ch.consume("accounts", processMsg, { noAck: true });
+        ch.consume("accounts", processMsg(browser, ch), { noAck: false });
         console.log("Worker is started");
       });
     });
   }
 
-  function processMsg(msg) {
-    console.log("PDF processing of ", msg.content.toString());
-  }
+  function processMsg(browser, ch) {
+      return (msg) => {
+        (async () => {
+            var myObject = await scrappAccount(browser, msg.content.toString());
+            console.log(myObject)
+            ch.ack(msg);
+        })();
+        }   
+    }
 
   function closeOnErr(err) {
     if (!err) return false;
@@ -55,4 +61,70 @@ function startWorker() {
     return true;
   }
 
-start();
+async function scrappAccount(browser, url){
+
+    var page = await browser.newPage()    
+    await page.goto('https://www.brainmap.ro' + url)
+
+    const details = {
+        'keywords':[],
+        'country': [],
+        'affiliations': [],
+        'roles':[],
+        'name':[]
+    }
+
+    const myfunction = async (text) => {
+        return await page.evaluate((text) => {
+            let results = [];
+            let items = document.querySelectorAll(text);
+            items.forEach((item) => {
+                let content = item.innerText.split(" | ")
+                results.push(...content);
+            });
+            return results;
+        }, text);
+    }
+
+    details.keywords = await myfunction('a.key_tag');
+    details.country = await myfunction("[id*=\"idTaraLucru\"]");
+    details.affiliations = await myfunction("div.profile-dash h5 label[id*=\"instNume\"]");
+    details.roles = await myfunction("label[id*=\"roles\"]");
+    details.name = await myfunction("[id*=\"Name\"]");
+
+    page.close()
+    return details;
+}
+(async () => {
+
+    // access brainmap.ro
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+          '--disable-extensions-except=/path/to/manifest/folder/',
+          '--load-extension=/path/to/manifest/folder/',
+        ]
+      });
+
+    //connect to RABITMQ
+    startRabbitMQ(browser);
+
+    // let urls = [
+    //     '/florin-pop',      
+    //     '/monica-turturean'
+    //   ];
+
+    // const accountInfo = [];
+   
+    // for (let i = 0; i < urls.length; i++) {
+    //     accountInfo.push(await scrappAccount(page, urls[i]));        
+    // };
+
+    // console.log(accountInfo)
+
+    // fs.writeFile('AccountInfo.json', accountInfo, function (err) {
+    // if (err) return console.log(err);
+    // // console.log('Hello World > helloworld.txt');
+    // });
+   })()
+
--- src/scrappAccount.js
+++ src/scrappAccount.js
@@ -1,87 +1,8 @@
 const puppeteer = require('puppeteer');
 fs = require('fs');
 
-
-(async () => {
-
-    //1 - Click On search to get the first page with profiles
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-          '--disable-extensions-except=/path/to/manifest/folder/',
-          '--load-extension=/path/to/manifest/folder/',
-        ]
-      });
-    const page = await browser.newPage()
-    let urls = [
-        '/florin-pop',      
-        '/monica-turturean',
-        '/irinel-casian-botez',
-        '/sergiu-coseri',
-        '/emil-alexandru-brujan',
-        '/tudor-luchian',
-        '/alexander-baumgarten',
-        '/bogdan-ichim',
-        '/gavril-sabau',
-        '/crisanta-alina-mazilescu',
-        '/liviu-marin',
-        '/catalin-croitoru',
-        '/dana-cristea',
-        '/gina-raluca-guse',
-        '/gigel-militaru',
-        '/mihai-oltean',
-        '/robert-ianos',
-        '/diana-trandabat',
-        '/goga-nicolae',
-        '/public-profile-8628',
-        '/mihail-ionescu',
-        '/iordache-virgil-alexandru',
-        '/simona-r-soare',
-        '/mihail-anton',
-        '/aurora-neagoe',
-        '/razvan-diaconescu',
-        '/mihaela-badea-doni',
-        '/lucretiu-birliba',
-        '/ruxandra-oana-jurcut',
-        '/cristinca-fulga',
-        '/carmen-buzea',
-        '/razvan-saftoiu',
-        '/raluca-nicoleta-balatchi',
-        '/cristian-oara',
-        '/ciprian-mihai-dobre',
-        '/frantz-daniel-fistung',
-        '/attila-bende',
-        '/gabriela-marinoschi',
-        '/bogdan-donose',
-        '/mircea-darabantu',
-        '/serban-peteu',
-        '/andreea-luiza-badin',
-        '/cristian-enache',
-        '/petre-ionita',
-        '/ligia-munteanu',
-        '/mihaela-tofan',
-        '/iuliana-aprodu',
-        '/emil-marin-popa',
-        '/ion-albulescu',
-        '/marcel-dirja',
-        '/miruna-mihaela-beldiman',
-        '/alpar-simon',
-        '/sebastian-burciu',
-        '/radu-fechete',
-        '/lucian-parvulescu',
-        '/public-profile-8616',
-        '/bogdan-ion',
-        '/public-profile-8619',
-        '/ionel-popa',
-        '/luminita-andronic'
-      ];
-
-    const accountInfo = [];
-   
-    for (let i = 0; i < urls.length; i++) {
-
-        pageURL = 'https://www.brainmap.ro' + urls[i]
-        await page.goto(pageURL)
+async function scrappAccount(page, url){
+        await page.goto('https://www.brainmap.ro' + url)
 
         const details = {
             'keywords':[],
@@ -109,15 +30,36 @@ fs = require('fs');
         details.roles = await myfunction("label[id*=\"roles\"]");
         details.name = await myfunction("[id*=\"Name\"]");
 
-        accountInfo.push(details);        
+        return details;
+}
+
+(async () => {
+
+    //1 - Click On search to get the first page with profiles
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+          '--disable-extensions-except=/path/to/manifest/folder/',
+          '--load-extension=/path/to/manifest/folder/',
+        ]
+      });
+    const page = await browser.newPage()
+    let urls = [
+        '/bogdan-marian-diaconu'
+      ];
+
+    const accountInfo = [];
+   
+    for (let i = 0; i < urls.length; i++) {
+        accountInfo.push(await scrappAccount(page, urls[i]));        
     };
 
     console.log(accountInfo)
 
-    fs.writeFile('AccountInfo.json', accountInfo, function (err) {
-    if (err) return console.log(err);
-    // console.log('Hello World > helloworld.txt');
-    });
+    // fs.writeFile('AccountInfo.json', accountInfo, function (err) {
+    // if (err) return console.log(err);
+    // // console.log('Hello World > helloworld.txt');
+    // });
 
     await browser.close()
    })()
